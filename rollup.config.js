6a4805f4e81487d917e8dfce3b96c7f501dcb502
rixo <rixo@rixo.fr>
2019-11-27 00:32:02 +0100

    test case for public serving path

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,24 @@ import hot from 'rollup-plugin-hot'
 import serve from 'rollup-plugin-serve'
 import staticFiles from 'rollup-plugin-static-files'
 
-const HOT = !!process.env.ROLLUP_WATCH
-const NOLLUP = !HOT
+const NOLLUP = !!process.env.NOLLUP
 
 const plugins = NOLLUP
   ? []
   : [
-      staticFiles({
-        include: ['./public'],
-      }),
       hot({
-        enabled: true,
         rewriteModuleHot: true,
-        public: 'dist',
+        public: 'public',
       }),
-      serve('dist'),
+      serve('public'),
     ]
 
 export default {
   input: './src/main.js',
   output: {
-    file: NOLLUP ? 'main.js' : 'dist/main.js',
-    format: 'esm',
+    sourcemap: 'inline',
+    file: 'public/build/main.js',
+    entryFileNames: 'public/build',
   },
   plugins,
 }
